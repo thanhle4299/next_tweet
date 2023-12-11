9eadf7c788a00c4fa3bfccad16f6165b1a8f1762
レー チー タイン <220071@yse.ac.jp>
2023-12-11 14:06:18 +0900

    20231211

---
+++ server/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Api\TweetController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+//ルーティングのURL：「/api/xxxx」
+// TweetController
+Route::get('tweet/get', [TweetController::class, 'get']);
+Route::post('tweet/add', [TweetController::class, 'add']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
